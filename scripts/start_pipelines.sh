64e5eed0f41bae232a320b923f6698f6b3cb4586
Martin Jackson <martjack@redhat.com>
2022-11-14 16:58:30 -0600

    Set up privileged cluster role for s2i pipeline

--- scripts/start_pipelines.sh
+++ scripts/start_pipelines.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -x
 
-apps=('quarkuscoffeeshop-barista' 'quarkuscoffeeshop-counter' 'quarkuscoffeeshop-counter' 'quarkuscoffeeshop-customermocker' 'quarkuscoffeeshop-inventory' 'quarkuscoffeeshop-kitchen' 'quarkuscoffeeshop-majestic-monolith' 'quarkuscoffeeshop-web' 'quarkuscoffeeshop-customerloyalty' 'homeoffice-backend' 'homeoffice-ingress' 'quarkuscoffeeshop-homeoffice-ui')
 namespace='quarkuscoffeeshop-cicd'
 
+# These apps build with the default pipeline
+apps=('quarkuscoffeeshop-barista' 'quarkuscoffeeshop-counter' 'quarkuscoffeeshop-counter' 'quarkuscoffeeshop-customermocker' 'quarkuscoffeeshop-inventory' 'quarkuscoffeeshop-kitchen' 'quarkuscoffeeshop-majestic-monolith' 'quarkuscoffeeshop-web' 'quarkuscoffeeshop-customerloyalty' 'homeoffice-backend' 'homeoffice-ingress')
 for app in ${apps[@]}; do
     pipeline="build-and-push-$app"
     mavenpvc="$app-maven-settings-pvc"
@@ -10,6 +11,22 @@ for app in ${apps[@]}; do
     gitresource="$app-git"
     imageresource="$app-image"
     tkn pipeline start $pipeline -n $namespace \
+        --resource 'app-git'="$gitresource" \
+        --resource image="$imageresource" \
+        --workspace name='shared-workspace',claimName="$workspacepvc" \
+        --workspace name='maven-settings',claimName="$mavenpvc" \
+        --use-param-defaults
+done
+
+# These apps require a privileged build pipeline
+privbuild_apps=('quarkuscoffeeshop-homeoffice-ui')
+for app in ${privbuild_apps[@]}; do
+    pipeline="build-and-push-$app"
+    mavenpvc="$app-maven-settings-pvc"
+    workspacepvc="$app-shared-workspace-pvc"
+    gitresource="$app-git"
+    imageresource="$app-image"
+    tkn pipeline start -s quarkuscoffeeshop-pipelines $pipeline -n $namespace \
         --resource 'app-git'="$gitresource" \
         --resource image="$imageresource" \
         --workspace name='shared-workspace',claimName="$workspacepvc" \
