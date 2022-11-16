949d864699d10a083094babd36899f8b2c7b2dd9
Martin Jackson <martjack@redhat.com>
2022-11-16 15:54:46 -0600

    Fix script - we need continue, not break

--- scripts/start_pipelines.sh
+++ scripts/start_pipelines.sh
@@ -14,19 +14,20 @@ while [ "$check" == "1" ]; do
         if [ "$?" != "0" ]; then
             echo "Error with pipeline $p, checking again"
             retry=1
-            break;
+            break
         fi
         retry=0
     done
 
     if [ "$retry" == "1" ]; then
-        break;
+        retry=0
+        continue
     fi
 
     oc get -n $build_ns secret quay-auth-secret 1>/dev/null 2>/dev/null
     if [ "$?" != "0" ]; then
         echo "Error with secret quay-auth-secret, checking again"
-        break;
+        continue
     fi
 
     # Everything checks out, time to leave this look
