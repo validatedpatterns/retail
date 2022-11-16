06e695b74d3e1c8ba134f3602b26d3ffb346988e
Martin Jackson <martjack@redhat.com>
2022-11-16 15:08:04 -0600

    Add breaks for pipeline start script

--- scripts/start_pipelines.sh
+++ scripts/start_pipelines.sh
@@ -4,19 +4,29 @@ build_ns=quarkuscoffeeshop-cicd
 pipelines=('build-and-push-homeoffice-backend' 'build-and-push-homeoffice-ingress' 'build-and-push-quarkuscoffeeshop-barista' 'build-and-push-quarkuscoffeeshop-counter' 'build-and-push-quarkuscoffeeshop-customerloyalty' 'build-and-push-quarkuscoffeeshop-customermocker' 'build-and-push-quarkuscoffeeshop-homeoffice-ui' 'build-and-push-quarkuscoffeeshop-inventory' 'build-and-push-quarkuscoffeeshop-kitchen' 'build-and-push-quarkuscoffeeshop-majestic-monolith' 'build-and-push-quarkuscoffeeshop-web')
 
 echo "Checking for resources to be available to start pipelines"
+retry=0
 check=1
 while [ "$check" == "1" ]; do
     sleep 2;
+
     for p in ${pipelines[@]}; do
         oc get -n $build_ns pipeline $p 1>/dev/null 2>/dev/null
         if [ "$?" != "0" ]; then
-            echo "Error with pipeline $p: $?"
+            echo "Error with pipeline $p, checking again"
+            retry=1
+            break;
         fi
+        retry=0
     done
+
+    if [ "$retry" == "1" ]; then
+        break;
+    fi
 
     oc get -n $build_ns secret quay-auth-secret 1>/dev/null 2>/dev/null
     if [ "$?" != "0" ]; then
-        echo "Missing secret quay-auth-secret: $?. Check vault and your secrets file."
+        echo "Error with secret quay-auth-secret, checking again"
+        break;
     fi
 
     # Everything checks out, time to leave this look
